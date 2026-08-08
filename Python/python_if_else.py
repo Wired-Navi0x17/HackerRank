9a5b7dc5d311849173706755690d8487a65a0c98
Wired-Navi0x1F <harshulkala14@gmail.com>
2026-08-08 17:03:30 +0530

    python_if_else.py

---
+++ Python/python_if_else.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+if (n % 2 != 0) :
+    print("Weird") 
+elif (2 <= n <= 5 or n > 20 ) :
+    print("Not Weird")
+else : 
+    print("Weird")
+
+    
+
